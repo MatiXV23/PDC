fcc652baf0a24de82f5f20260793dda77a00d845
Agustin <agustincigaran@icloud.com>
2025-09-10 17:23:51 -0300

    isOwner archive

---
+++ src/decorators/authentication/isOwner.ts
@@ -0,0 +1,19 @@
+import fastifyPlugin from "fastify-plugin";
+import type { Usuario } from "../../models/usuario_model.ts";
+import { PC_Forbidden, PC_InternalServerError, PC_NoAuthorized } from "../../errors/errors.ts";
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export default fastifyPlugin(async function (fastify) {
+  fastify.decorate("isOwner", (req: FastifyRequest, rep: FastifyReply) => {
+    const usuario = (req as any).user
+                console.log(usuario)
+                if (!(req.params.id_usuario === usuario.id_usuario)) throw new PC_Forbidden()
+    return
+  });
+});
+
+declare module "fastify" {
+  interface FastifyInstance {
+    isOwner(req: FastifyRequest, rep: FastifyReply): void;
+  }
+}
