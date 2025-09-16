aff3e03c62e01bef98d3581af3c0840f2f4bf9e7
Mati <mperezfrontan@gmail.com>
2025-09-16 20:54:16 -0300

    cambios en BD

--- src/services/usuarios_db_service.ts
+++ src/services/usuarios_db_service.ts
@@ -1,25 +1,25 @@
-import { BaseRepository } from "../models/baseRepository.ts";
+import { BasePgRepository } from "../models/baseRepository.ts";
 import  type { Usuario, Credenciales } from "../models/usuario_model.ts";
-import fastify, { type FastifyInstance } from "fastify";
 import { PC_BadRequest, PC_InternalServerError, PC_NotFound, PC_NotImplemented } from "../errors/errors.ts";
+import { Pool } from "pg";
+import { Any } from "@fastify/type-provider-typebox";
 
+export class UsuariosDB extends BasePgRepository<Usuario> {
 
-export class UsuariosDB extends BaseRepository<Usuario> {
-    #baseQuery = `SELECT 
+    constructor(pool: Pool) {
+        super(pool)
+    }
+
+    #baseQuery = /*sql*/`
+                SELECT 
                     u.*, array_agg(r.nombre) AS roles
                 FROM usuarios u
                 LEFT JOIN usuarios_roles ru ON u.id_usuario = ru.id_usuario
                 LEFT JOIN roles r ON ru.id_rol = r.id_rol
                 `
 
-    constructor(fastify: FastifyInstance) {
-        super(fastify)
-    }
-
-    
-
     async getAll(): Promise<Usuario[]> {
-        const users = await this.pg.query<Usuario>(this.#baseQuery+'GROUP BY u.id_usuario;')
+        const users = await this.pool.query<Usuario>(this.#baseQuery+'GROUP BY u.id_usuario;')
 
         return users.rows
     }
@@ -28,45 +28,39 @@ export class UsuariosDB extends BaseRepository<Usuario> {
         const query = this.#baseQuery + `WHERE u.id_usuario = $1
                                         GROUP BY u.id_usuario;`
         const vars = [id]
-        const res = await this.pg.query<Usuario>(query, vars)
+        const res = await this.pool.query<Usuario>(query, vars)
         
         if (res.rowCount === 0) throw new PC_NotFound(`Usuario con id (${id}) no encontrado`)
         return res.rows[0]
     }
 
     async create(data: Partial<Usuario>): Promise<Usuario> {
-        let query = ` WITH nuevo_usuario AS (
+        const {username, email, activo, reputacion, fecha_nacimiento, nombres, apellidos, edad, sexo, foto_url, roles} = data
+        const rolsQ = roles?.includes('admin') ? `('user', 'admin')` : `('user')`
+        let query = /*sql*/` 
+                    WITH nuevo_usuario AS (
                         INSERT INTO usuarios (username, email, activo, reputacion, fecha_nacimiento, nombres, apellidos, edad, sexo, foto_url)
-                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-                        RETURNING id_usuario
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+                            RETURNING id_usuario
                         ),
                         cred AS (
-                        INSERT INTO credenciales (id_usuario, password_hash)
-                        SELECT id_usuario, crypt('miPasswordSeguro', gen_salt('bf'))
-                        FROM nuevo_usuario
-                        RETURNING id_usuario
+                            INSERT INTO credenciales (id_usuario, password_hash)
+                            SELECT id_usuario, crypt('contraseña', gen_salt('bf'))
+                            FROM nuevo_usuario
+                            RETURNING id_usuario
                         ),
                         roles AS (
-                        INSERT INTO usuarios_roles (id_usuario, id_rol)
-                        SELECT c.id_usuario, r.id_rol
-                        FROM cred c, roles r
-                        WHERE r.nombre = 'user'`
-        if (data.roles?.includes('admin')) query += `OR r.nombre = 'admin'`
-        query += `  RETURNING id_usuario)
+                            INSERT INTO usuarios_roles (id_usuario, id_rol)
+                            SELECT c.id_usuario, r.id_rol
+                            FROM cred c
+                            JOIN roles r ON r.nombre IN ${rolsQ}
+                            RETURNING id_usuario
+                        )
                     SELECT id_usuario from roles;`
 
-        const {username, email, activo, reputacion, fecha_nacimiento, nombres, apellidos, edad, sexo, foto_url, roles} = data
-        try{
-            const res = await this.pg.query(query, [username, email, activo, reputacion, fecha_nacimiento, nombres, apellidos, edad, sexo, foto_url || null])
-
-            console.log(res)
-
-            const user:Usuario = await this.getById(res.row[0].id_usuario)
-            return user
-        }
-        catch (err){
-            throw new PC_InternalServerError(err.detail)
-        }
+        const res = await this.pool.query(query, [username, email, activo, reputacion, fecha_nacimiento, nombres, apellidos, edad, sexo, foto_url || null])
+        const user:Usuario = await this.getById(res.rows[0].id_usuario)
+        return user
     }
 
     async update(id: number, data: Partial<Usuario>): Promise<Usuario> {
@@ -90,7 +84,7 @@ export class UsuariosDB extends BaseRepository<Usuario> {
        
         console.log(query)
         console.log(vars)
-        const res = await this.pg.query(query, vars)
+        const res = await this.pool.query(query, vars)
 
 
         return await this.getById(id)
@@ -99,7 +93,7 @@ export class UsuariosDB extends BaseRepository<Usuario> {
     async delete(id: number): Promise<void> {
         const query =  `DELETE FROM usuarios
                         WHERE id_usuario = $1;`
-        const res = await this.pg.query<Usuario>(query, [id])
+        const res = await this.pool.query<Usuario>(query, [id])
 
         if (res.rowCount === 0) throw new PC_BadRequest(`Usuario de id ${id}, no existe. Ignorando`)
         console.log(res)
@@ -118,7 +112,7 @@ export class UsuariosDB extends BaseRepository<Usuario> {
                                         WHERE u.username = $1 AND c.password_hash = crypt($2, password_hash)
                                         GROUP BY u.id_usuario;`
         const vars = [credenciales.username, credenciales.password]
-        const res = await this.pg.query<Usuario>(query, vars)
+        const res = await this.pool.query<Usuario>(query, vars)
         
         if (res.rowCount === 0) throw new PC_NotFound(`Usuario no encontrado. Credenciales Incorrectas`)
 
