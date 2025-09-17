48babac4167ca9716ea80ee499acf1e402220e8d
brahnu <nunezbrahian@gmail.com>
2025-09-16 22:53:13 -0300

    return original

--- src/models/usuario_model.ts
+++ src/models/usuario_model.ts
@@ -2,18 +2,18 @@ import { Type } from '@sinclair/typebox'
 import type { Static } from '@sinclair/typebox'
 
 export const usuarioSchema = Type.Object({
-    id_usuario: Type.Integer({minimum: 1}),
-    username: Type.String({maximum: 15}),
+    id_usuario: Type.Number({minimum: 1}),
+    username: Type.String({maxLength: 15}),
     email: Type.String(),
     activo: Type.Boolean(),
     reputacion: Type.Number({minimum: 0, maximum: 999}),
     fecha_registro: Type.String({ format: 'date-time'}),
     fecha_nacimiento: Type.String({ format: 'date'}),
-    nombres: Type.String({maximum:50}),
-    apellidos: Type.String({maximum:50}),
-    edad: Type.Integer(),
+    nombres: Type.String({maxLength:50}),
+    apellidos: Type.String({maxLength:50}),
+    edad: Type.Number(),
     sexo: Type.String({enum: ["M", "F"]}),
-    foto_url: Type.Optional(Type.String({maximum:520})),
+    foto_url: Type.Optional(Type.String({maxLength:520})),
     roles: Type.Array(Type.String({enum: ["user", "admin"]}))
 })
 
@@ -33,4 +33,6 @@ export type Credenciales = Static<typeof credencialesSchema>
 export type Usuario = Static<typeof usuarioSchema>
 
 
-
+export type UsuarioM = Usuario & {
+    isAdmin() : () => boolean
+}
